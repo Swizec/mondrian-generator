6181e52baefe1ae6ebcde6219f7a7f7ee49b3203
Swizec Teller <swizec@swizec.com>
2019-11-03 15:24:11 -0800

    Use generative params for colors

--- src/Mondrian.js
+++ src/Mondrian.js
@@ -2,49 +2,65 @@ import React from "react";
 import * as d3 from "d3";
 import { hierarchy } from "d3-hierarchy";
 
-const data = {
-    value: 1,
-    children: [
-        { value: 1 },
-        {
-            value: 1,
-            children: [{ value: 1 }, { value: 1 }]
-        },
-        {
-            value: 1,
-            children: [
-                { value: 1 },
-                { value: 1 },
-                {
-                    value: 1,
-                    children: [{ value: 1 }, { value: 1 }]
-                }
-            ]
-        }
-    ]
+// const data = {
+//     value: 1,
+//     children: [
+//         { value: 1 },
+//         {
+//             value: 1,
+//             children: [{ value: 1 }, { value: 1 }]
+//         },
+//         {
+//             value: 1,
+//             children: [
+//                 { value: 1 },
+//                 { value: 1 },
+//                 {
+//                     value: 1,
+//                     children: [{ value: 1 }, { value: 1 }]
+//                 }
+//             ]
+//         }
+//     ]
+// };
+
+// Create weighted probability distribution to pick a random color for a square
+const createColor = ({ red, blue, yellow, black }) => {
+    const probabilitySpace = [
+        ...new Array(red * 10).fill("red"),
+        ...new Array(blue * 10).fill("blue"),
+        ...new Array(yellow * 10).fill("yellow"),
+        ...new Array(black * 10).fill("black"),
+        ...new Array(red * 10 + blue * 10 + yellow * 10 + black * 10).fill(
+            "#fffaf1"
+        )
+    ];
+
+    return d3.shuffle(probabilitySpace)[0];
+};
+
+const generateMondrian = ({ value, depth = 0 }) => {
+    const N = Math.round(Math.random() * 6);
+
+    return {
+        value,
+        color: createColor({ red: 0.3, blue: 0.2, yellow: 0.6, black: 0.4 }),
+        children:
+            depth < 2
+                ? d3.range(N).map(_ =>
+                      generateMondrian({
+                          value: value / N,
+                          depth: depth + 1
+                      })
+                  )
+                : null
+    };
 };
 
 const MondrianRectangle = ({ node }) => {
     const { x0, y0, x1, y1, children } = node,
         width = x1 - x0,
         height = y1 - y0;
-
-    const color = () => {
-        const ratio = width > height ? height / width : width / height;
-        if (!node.parent) {
-            // Root node is black
-            return "black";
-        } else if (ratio >= 0.9) {
-            return "red";
-        } else if (ratio >= 0.7) {
-            return "blue";
-        } else if (ratio >= 0.5) {
-            return "yellow";
-        } else {
-            // default is off white
-            return "#fffaf1";
-        }
-    };
 
     return (
         <>
@@ -54,8 +70,9 @@ const MondrianRectangle = ({ node }) => {
                 width={width}
                 height={height}
                 style={{
-                    fill: color(),
-                    stroke: "black"
+                    fill: node.data.color,
+                    stroke: "black",
+                    strokeWidth: 5
                 }}
             />
             {children &&
@@ -70,8 +87,10 @@ const Mondrian = ({ x, y, width, height }) => {
     const treemap = d3
         .treemap()
         .size([width, height])
-        .padding(10)
+        .padding(5)
         .tile(d3.treemapBinary);
+
+    const data = generateMondrian({ value: 100 });
 
     const root = treemap(
         hierarchy(data)
